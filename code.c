ac19063aa96b7af37066efe9758a5fd334ef24dc
shivam kumar singh <89032612+shivam01091@users.noreply.github.com>
2022-05-29 23:02:34 +0530

    Create code.c

---
+++ code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%4==0){
+        printf("ITS A SPECIAL NUMBER");
+    }else{
+        printf("Its not a special no.");
+    }
+    return 0;
+}
